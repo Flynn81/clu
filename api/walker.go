944e60eb0ac5191f19735bf97e4a65fd5195964b
flynn81 <tom.karam@gmail.com>
2024-10-17 21:44:41 -0400

    getting basic functions for walking directories implemented sans tests

--- api/walker.go
+++ api/walker.go
@@ -6,16 +6,22 @@ import (
 	"path/filepath"
 )
 
-func visitDirectory(path string, d fs.DirEntry, err error) error {
-	i,_ := d.Info() //todo handle error
-	fmt.Printf("visited: %v\n", i)
-	
-	return nil
-}
+var fileCount int = 1
 
 type BasicWalker struct{}
 
+func visitDirectory(path string, d fs.DirEntry, err error) error {
+	i,_ := d.Info() //todo handle error
+	fmt.Printf("visited: %v\n", i.Name())
+	fileCount++
+	
+	//naive approach, have another function that updates an init var to count totals / keep track of dir, names etc
+
+	return nil
+}
+
 func (w BasicWalker) Walk(path string) (int64, []int64, []string) {
 	filepath.WalkDir(path, visitDirectory)
+	fmt.Printf("fileCount %v\n", fileCount)
 	return 0, nil, nil
 }
--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func main() {
 	fmt.Println("Hello, World!")
 
 	w := api.BasicWalker{}
-	w.Walk(".")
+	w.Walk("./")
 }
